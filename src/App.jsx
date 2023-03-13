c94385450ebb482a1f4a3e8c84a5bc0e8402f2e4
Lucas Piantini <lucaspiantini@gmail.com>
2023-03-13 09:41:46 -0300

    maqueta del proyecto

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { PokeGrid } from "./components/home/PokeGrid";
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { PokeDetails } from "./components/details/PokeDetails";
+
+function App() {
+
+    const router = createBrowserRouter([
+    {
+        path:'/',
+        element: <PokeGrid/>
+    },
+    {
+        path:'/details/:id',
+        element: <PokeDetails />
+    }
+]);
+
+  return (
+      <div className="App">
+        <RouterProvider router={router} />
+      </div>
+  );
+}
+
+export default App;
---
+++ src/components/details/PokeDetails.jsx
@@ -0,0 +1,30 @@
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+import { CardActionArea } from "@mui/material";
+import { useParams } from "react-router-dom";
+
+export const PokeDetails = () => {
+  const params = useParams();
+
+  return (<Card>
+    <CardActionArea>
+      <CardMedia
+        component="img"
+        height="400"
+        image={""}
+        alt="photo"
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          {""}
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          { }
+        </Typography>
+      </CardContent>
+    </CardActionArea>
+  </Card>
+  );
+};
