b2886514f836e526a5c78a0e2be824530f6906e6
Lucas Piantini <lucaspiantini@gmail.com>
2023-03-14 11:41:56 -0300

    se termino el desarrollo ahora falta estilizar

--- src/components/details/PokeDetails.jsx
+++ src/components/details/PokeDetails.jsx
@@ -4,28 +4,31 @@ import CardMedia from "@mui/material/CardMedia";
 import Typography from "@mui/material/Typography";
 import { CardActionArea } from "@mui/material";
 import { useParams } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 export const PokeDetails = () => {
   const params = useParams();
-  console.log(params)
+  const pokemonList = useSelector(state => state.pokemons.list)
+  const pokemon = pokemonList.find((item) => item.id == params.id);
 
-  return (<Card>
-    <CardActionArea>
-      <CardMedia
-        component="img"
-        height="400"
-        image={""}
-        alt="photo"
-      />
-      <CardContent>
-        <Typography gutterBottom variant="h5" component="div">
-          {""}
-        </Typography>
-        <Typography variant="body2" color="text.secondary">
-          { }
-        </Typography>
-      </CardContent>
-    </CardActionArea>
-  </Card>
-  );
+  return (
+    <Card>
+      <CardActionArea>
+        <CardMedia
+          component="img"
+          height="400"
+          image={pokemon.sprites.front_default}
+          alt="photo"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            {pokemon.name}
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+          {pokemon.types.map((type) => type.type.name + " ")}
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+    </Card>
+  )
 };
